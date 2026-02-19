0b5e37a73f3257a4faa3681d253541cbd15c8734
Tiago Albuquerque Ferreira <tiagoferreira@MacBook-Air-de-Tiago.local>
2026-02-19 09:54:07 -0300

    Create services for auth

---
+++ src/services/auth.ts
@@ -0,0 +1,38 @@
+import type { PrismaUserRepository } from '@/repositories/prisma-user-repository.js'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error.js'
+import { compare } from 'bcryptjs'
+import type { User } from '@prisma/client'
+
+interface AuthServiceRequest {
+  email: string
+  password: string
+}
+
+interface AuthServiceReply {
+  user: User
+}
+
+export class AuthService {
+  constructor(private usersRespository: PrismaUserRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthServiceRequest): Promise<AuthServiceReply> {
+    const user = await this.usersRespository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatch = compare(password, user.password_hash)
+
+    if (!doesPasswordMatch) {
+      throw new InvalidCredentialsError()
+    }
+
+    return {
+      user,
+    }
+  }
+}
