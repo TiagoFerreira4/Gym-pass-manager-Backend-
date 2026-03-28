9141bb95d68b14535f0033ec6717356cd21fcd1d
Tiago Ferreira <tiago.ferreira0410@gmail.com>
2026-03-28 10:38:01 -0300

    config: tests environments (unit & e2e)
    '

---
+++ src/vite.config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    dir: 'src',
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: 'unit',
+          dir: 'src/services',
+          environment: 'node'
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'e2e',
+          dir: 'src/http/controllers',
+          environment: './prisma/vitest-env-prisma/prisma-test-env.ts'
+        }
+      }
+    ]
+  },
+
+})
