0243fa5bfd294d65ccfd35b5434d2074b6c347ed
Tiago Ferreira <tiago.ferreira0410@gmail.com>
2026-03-31 18:10:31 -0300

    feat: role based authorization

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -4,6 +4,7 @@ import { verifyJWT } from '../../middlewares/verify-jwt.js'
 import { search } from './search.js'
 import { nearbyGyms } from './nearby.js'
 import { create } from './create.js'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role.js'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
@@ -11,5 +12,5 @@ export async function gymsRoutes(app: FastifyInstance) {
   app.get('/gyms/search', search)
   app.get('/gyms/nearby', nearbyGyms)
 
-  app.post('/gyms', create)
+  app.post('/gyms', { onRequest: [verifyUserRole('ADMIN')] }, create)
 }
